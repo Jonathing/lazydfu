a4c7cbbd740dbbd1fad200f589b07abfc13cb5ba
Jonathan Colmenares <jonathing.projects@gmail.com>
2021-08-18 21:00:04 -0400

    convert the mixin into a coremod

---
+++ src/main/resources/lazydfu.coremod.js
@@ -0,0 +1,35 @@
+// class imports
+var Opcodes = Java.type('org.objectweb.asm.Opcodes');
+
+function initializeCoreMod() {
+    return {
+        'createFixerUpper': {
+            'target': {
+                'type': 'METHOD',
+                'class': 'net.minecraft.util.datafix.DataFixesManager',
+                'methodName': 'func_188279_a',
+                'methodDesc': '()Lcom/mojang/datafixers/DataFixer;'
+            },
+            'transformer': createFixerUpper
+        }
+    }
+}
+
+// Change all calls of "new DataFixerBuilder()" to "new LazyDataFixerBuilder()"
+function createFixerUpper(method) {
+    var i;
+    for (i = 0; i < method.instructions.size(); i++) {
+        var insn = method.instructions.get(i)
+
+        if (insn.getOpcode() == Opcodes.NEW && insn.desc.equals('com/mojang/datafixers/DataFixerBuilder')) {
+            insn.desc = 'me/steinborn/lazydfu/mod/LazyDataFixerBuilder'
+        }
+
+        if (insn.getOpcode() == Opcodes.INVOKESPECIAL && insn.owner.equals('com/mojang/datafixers/DataFixerBuilder') && insn.name.equals('<init>')) {
+            insn.owner = 'me/steinborn/lazydfu/mod/LazyDataFixerBuilder'
+        }
+    }
+
+    // finish up
+    return method;
+}
