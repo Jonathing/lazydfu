40a7da9ff53aef57e5f3ecbe9c901e4d0a727467
Jonathan Colmenares <jonathing.projects@gmail.com>
2021-10-14 15:03:34 -0400

    braces on newline. why did I use the baby way

--- src/main/resources/coremods/DataFixersCoreMod.js
+++ src/main/resources/coremods/DataFixersCoreMod.js
@@ -8,7 +8,8 @@ var Opcodes = Java.type('org.objectweb.asm.Opcodes')
  *
  * @returns {{createFixer: {transformer: (function(*=): *), target: {type: string, name: string}}}}
  */
-function initializeCoreMod() {
+function initializeCoreMod()
+{
     return {
         'createFixer': {
             'target': {
@@ -28,7 +29,8 @@ function initializeCoreMod() {
  * @param clazz The DataFixesManager class's bytecode that will be transformed by this coremod.
  * @returns {*} The transformed class.
  */
-function createFixer(clazz) {
+function createFixer(clazz)
+{
     // Get the MethodNode we're trying to inject into.
     var method = getMethod(clazz, ASMAPI.mapMethod("func_188279_a"))
 
@@ -38,31 +40,39 @@ function createFixer(clazz) {
     var invokeCount = 0
 
     // Start reading from the head of the MethodNode.
-    for (i = 0; i < method.instructions.size(); i++) {
+    for (i = 0; i < method.instructions.size(); i++)
+    {
         var insn = method.instructions.get(i)
 
         // Replace any creations of DataFixerBuilder with LazyDataFixerBuilder.
-        if (insn.getOpcode() == Opcodes.NEW && insn.desc.equals('com/mojang/datafixers/DataFixerBuilder')) {
+        if (insn.getOpcode() == Opcodes.NEW && insn.desc.equals('com/mojang/datafixers/DataFixerBuilder'))
+        {
             insn.desc = 'me/steinborn/lazydfu/mod/LazyDataFixerBuilder'
             newCount++
         }
 
         // Replace any invocations of "new DataFixerBuilder()" with "new LazyDataFixerBuilder()".
-        if (insn.getOpcode() == Opcodes.INVOKESPECIAL && insn.owner.equals('com/mojang/datafixers/DataFixerBuilder') && insn.name.equals('<init>')) {
+        if (insn.getOpcode() == Opcodes.INVOKESPECIAL && insn.owner.equals('com/mojang/datafixers/DataFixerBuilder') && insn.name.equals('<init>'))
+        {
             insn.owner = 'me/steinborn/lazydfu/mod/LazyDataFixerBuilder'
             invokeCount++
         }
     }
 
     // Check if anything might've gone wrong early on.
-    if (newCount == 1 && invokeCount == 1) {
+    if (newCount == 1 && invokeCount == 1)
+    {
         log('INFO', '[LazyDFU] LazyDFU was initialized successfully.')
-    } else if (newCount == 0 || invokeCount == 0) {
+    }
+    else if (newCount == 0 || invokeCount == 0)
+    {
         log('FATAL', '[LazyDFU] LazyDFU seems to have been initialized successfully, but something seems off.')
         log('FATAL', '[LazyDFU] Any variable trying to create a normal DataFixerBuilder did not exist at the time of method transformation.')
         log('FATAL', '[LazyDFU] This usually means another mod is trying to kill or modify the data fixer initialization system.')
         log('FATAL', '[LazyDFU] Please avoid using mods alongside LazyDFU that do this such as DataBreaker, DataFixerSlayer, or RandomPatches\'s data fixer disabler.')
-    } else {
+    }
+    else
+    {
         log('FATAL', '[LazyDFU] LazyDFU seems to have been initialized successfully, but something seems off.')
         log('FATAL', '[LazyDFU] It seems like more than one DataFixerBuilder was transformed in the method, which should be impossible.')
         log('FATAL', '[LazyDFU] In any case, please avoid using mods alongside LazyDFU that do this such as DataBreaker, DataFixerSlayer, or RandomPatches\'s data fixer disabler.')
@@ -79,10 +89,13 @@ function createFixer(clazz) {
  * @param name  The name of the method to get the MethodNode for.
  * @returns {*} The MethodNode that was found in the ClassNode.
  */
-function getMethod(clazz, name) {
-    for (var index in clazz.methods) {
+function getMethod(clazz, name)
+{
+    for (var index in clazz.methods)
+    {
         var method = clazz.methods[index]
-        if (method.name.equals(name)) {
+        if (method.name.equals(name))
+        {
             return method
         }
     }
@@ -97,10 +110,14 @@ function getMethod(clazz, name) {
  * @param level   The logging level to log to.
  * @param message The message to log to the console.
  */
-function log(level, message) {
-    try {
+function log(level, message)
+{
+    try
+    {
         ASMAPI.log(level, message)
-    } catch (error) {
+    }
+    catch (error)
+    {
         print(message)
     }
 }
