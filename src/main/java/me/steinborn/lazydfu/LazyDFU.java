ea090d3a94bdec759d43843ba25b563239e01b57
Jonathan Colmenares <jonathing.projects@gmail.com>
2021-10-22 10:00:20 -0400

    display a warning if lazydfu didn't work

--- src/main/java/me/steinborn/lazydfu/LazyDFU.java
+++ src/main/java/me/steinborn/lazydfu/LazyDFU.java
@@ -1,16 +1,49 @@
 package me.steinborn.lazydfu;
 
+import net.minecraftforge.fml.*;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.forgespi.language.IModInfo;
+
+import java.util.ConcurrentModificationException;
 
 /**
  * This is the main class of the LazyDFU mod and it does... well... nothing! It merely exists as a placeholder so that
  * Forge is able to detect our JAR file and can then load our coremod into the game.
  *
+ * @author Jonathing
  * @author CorgiTaco
  * @see me.steinborn.lazydfu.mod.LazyDataFixerBuilder
  * @since 0.1.3
  */
-@Mod("lazydfu")
+@Mod(LazyDFU.MOD_ID)
 public class LazyDFU
 {
+    public static final String MOD_ID = "lazydfu";
+
+    public static boolean isUsingLazyBuilder = false;
+
+    public LazyDFU()
+    {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+        if (!isUsingLazyBuilder)
+        {
+            IModInfo modInfo =  ModList.get().getModContainerById(LazyDFU.MOD_ID).get().getModInfo();
+
+            ConcurrentModificationException e = new ConcurrentModificationException(ForgeI18n.parseMessage("lazydfu.warning.exception"));
+
+            ModLoadingWarning warning = new ModLoadingWarning(
+                    modInfo, ModLoadingStage.COMMON_SETUP, "lazydfu.warning.message",
+                    e.getMessage(),
+                    e.getClass().getSimpleName()
+            );
+
+            ModLoader.get().addWarning(warning);
+        }
+    }
 }
--- src/main/java/me/steinborn/lazydfu/mod/LazyDataFixerBuilder.java
+++ src/main/java/me/steinborn/lazydfu/mod/LazyDataFixerBuilder.java
@@ -1,6 +1,8 @@
 package me.steinborn.lazydfu.mod;
 
-import com.mojang.datafixers.*;
+import com.mojang.datafixers.DataFixer;
+import com.mojang.datafixers.DataFixerBuilder;
+import me.steinborn.lazydfu.LazyDFU;
 
 import java.util.concurrent.Executor;
 
@@ -8,24 +10,30 @@ import java.util.concurrent.Executor;
  * This version of {@code DataFixerBuilder} does not immediately initialize rules.
  *
  * @author tuxed
+ * @author Jonathing
  * @since 0.1.0
  */
 @SuppressWarnings("unused")
 public class LazyDataFixerBuilder extends DataFixerBuilder
 {
+    //@formatter:off
     private static final Executor NO_OP_EXECUTOR = command -> {};
+    //@formatter:on
 
     /**
      * This constructor method initializes the lazy data fixer with the given data version.
      *
      * @param dataVersion The data version (usually representative of the Minecraft version) for the DFU to use.
      */
-    public LazyDataFixerBuilder(int dataVersion) {
+    public LazyDataFixerBuilder(int dataVersion)
+    {
         super(dataVersion);
     }
 
     @Override
-    public DataFixer build(Executor executor) {
+    public DataFixer build(Executor executor)
+    {
+        LazyDFU.isUsingLazyBuilder = true;
         return super.build(NO_OP_EXECUTOR);
     }
 }
